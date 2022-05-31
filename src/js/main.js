88e780063b4e49194cce5f5a9e2c51c1f03b6b1e
Raloseq <45914275+Raloseq@users.noreply.github.com>
2022-05-31 21:48:16 +0200

    added table with fetched data from endpoint

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ const closeModal = document.querySelector('.modal__close');
 const bgModal = document.querySelector('.modal__bg');
 const spanTimesClicked = document.querySelector('.modal__span');
 const resetClicks = document.querySelector('.modal__reset-clicks');
+const modalTable = document.querySelector('.modal__table');
 
 const timesClicked = localStorage.getItem('timesClicked');
 
@@ -35,3 +36,30 @@ resetClicks.addEventListener('click', () => {
     spanTimesClicked.textContent = counter;
     resetClicks.classList.remove('active-reset-btn');
 })
+
+const getData = () => {
+    const url = `https://jsonplaceholder.typicode.com/users`;
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let dataTable = "";
+            data.forEach(e => {
+                const address = `${e.address.city}${e.address.street}${e.address.suite}`;
+                
+                dataTable += `
+                <tr>
+                        <td>${e.name}</td>
+                        <td>${e.email}</td>
+                        <td>${address}</td>
+                        <td>${e.phone}</td>
+                        <td>${e.company.name}</td>
+                </tr>
+                `;
+            });
+            modalTable.innerHTML += dataTable;
+        })
+
+}
+
+getData();
